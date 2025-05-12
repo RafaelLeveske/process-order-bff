d09cc2ab862e2109d3b6a66ae4fa50f899af6876
RafaelLeveske <rafaelleveske@gmail.com>
2025-05-12 15:23:15 -0300

    feat: implement serverless offline

--- api.ts
+++ api.ts
@@ -1,5 +1,5 @@
-import "./src/infra/database/mongodb/connections";
-import APIHandler from "./src/handlers/APIHandler";
-import { storeRequest } from "./src/infra/functions/ApiGateway/storeRequest";
+import 'reflect-metadata'
+import APIHandler from "./src/modules/orders/handlers/APIHandler";
+import { storeRequest } from "./src/modules/orders/infra/functions/ApiGateway/storeRequest";
 
 export const storeRequestAPI = APIHandler(storeRequest);
